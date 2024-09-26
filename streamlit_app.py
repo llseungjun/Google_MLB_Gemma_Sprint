272407e873f9365c5c347acce4fad592ce8e94a2
llseungjun <sjl27552755@gmail.com>
2024-09-26 15:56:03 +0900

    gitignore - data 폴더 추가 streamlit_app - 주석 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,7 +137,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0) # vllm으로 serving된 llm을 해당 부분에 집어넣기
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
